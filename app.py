99cb7f6380ecb3bddd259fc7c36549ae7f3621c2
moazAlhomsi <93142575+moazAlhomsi@users.noreply.github.com>
2023-02-12 05:27:39 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,89 @@
+import streamlit as st
+from app1 import logistic_regression, random_forest, naive_bayes
+from streamlit_option_menu import option_menu
+from streamlit_lottie import st_lottie
+import json
+
+
+st.set_page_config(page_title="Breast Cancer", page_icon="⚕️", layout="wide")
+
+
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
+
+
+def lottie(file_path=str):
+    with open(file_path, "r") as f:
+        return json.load(f)
+
+
+
+st.markdown("""
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+""", unsafe_allow_html=True)
+
+
+st.markdown("""<h1 style="font-family: 'Times New Roman', Times, serif; font-size: 40px; color: #ffccff; font-weight: bold;">Breast Cancer Prediction</h1>""", unsafe_allow_html=True)
+
+lotfile = lottie("lottiefiles/cancer.json")
+
+    
+
+navi = option_menu(
+    menu_title="ML Models",
+    options=["Home","Logistic Regression", "Random Forest", "GaussianNB"],
+    icons = ["house","dot", "dot", "dot"],
+    orientation="horizontal")  
+
+if navi == "Home":
+    st.write("""""")
+    col1, col2 = st.columns(2)
+    with col1:
+        st_lottie(
+                lotfile,
+                speed=1,
+                loop=True,
+                reverse=False,
+                quality="low",
+                height=500,
+                width=900,
+                key=None
+            )
+    with col2:
+        st.write("""
+___________________________________________________________________________________________________________________________________
+
+
+
+## Breast Cancer Overview:
+
+Breast cancer is a type of cancer that affects the breast tissue, and it is the most common cancer among women worldwide. Early detection and treatment are crucial for survival. Some common symptoms of breast cancer include a lump or thickening in the breast or underarm, change in size or shape of the breast, skin dimpling, nipple discharge, and redness or scaling of the nipple or skin (National Cancer Institute, 2021).
+
+The exact cause of breast cancer is unknown, but several risk factors have been identified, including age, family history of breast cancer, exposure to estrogen, genetic mutations, and personal history of breast or ovarian cancer (National Cancer Institute, 2021).
+
+Treatment options for breast cancer depend on the stage and type of cancer, and may include surgery, radiation therapy, chemotherapy, hormonal therapy, and targeted therapy (National Cancer Institute, 2021).
+
+Reference:
+
+National Cancer Institute. (2021). Breast Cancer Treatment (PDQ) - Health Professional Version. Retrieved from https://www.cancer.gov/types/breast/patient/breast-treatment-pdq""")
+
+
+ 
+if navi == "Logistic Regression":
+    logistic_regression()
+    
+
+if navi == "Random Forest":
+    random_forest()
+
+
+if navi == "GaussianNB":
+    naive_bayes()
+    
+
---
+++ app1.py
@@ -0,0 +1,239 @@
+import streamlit as st
+from sklearn import datasets
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import confusion_matrix, accuracy_score, classification_report, RocCurveDisplay, PrecisionRecallDisplay
+import matplotlib.pyplot as plt
+plt.rcParams['figure.figsize'] = (12, 8)
+
+breast_cancer = datasets.load_breast_cancer()
+x = pd.DataFrame(breast_cancer.data, columns=breast_cancer.feature_names)
+y = pd.Categorical.from_codes(breast_cancer.target, breast_cancer.target_names)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=42)
+
+# -----------------------------------------------------
+# ---Logistic Regression---
+# -----------------------------------------------------
+
+def logistic_regression():
+    lr = LogisticRegression()
+    lr.fit(x_train, y_train)
+    y_pred_test = lr.predict(x_test)
+    y_pred_train = lr.predict(x_train)
+    
+
+    st.write("____________________________________________________________________________________________________________________")
+    st.markdown("""<h1 style="font-family: 'Times New Roman', Times, serif; font-size: 40px; color: #ff4d4d; font-weight: bold;">Model Evaluation</h1>""", unsafe_allow_html=True)
+    st.write("____________________________________________________________________________________________________________________")
+    st.container()
+    col1 , col2 = st.columns((3,2))
+    with col1:
+        # st.markdown("## Acuracy on test set:")
+        st.metric(label="Acuracy on test set", value="{:.2%}".format(round(accuracy_score(y_test, y_pred_test),3)))
+        if accuracy_score(y_test, y_pred_test) >= 0.9:
+            st.write("The accuracy score is high and shows a great prediction power")
+
+    with col2:
+        # st.markdown("## Acuracy on training set")
+        # st.write("{:.2%}".format(round(accuracy_score(y_train, y_pred_train),3)))
+        st.metric(label="Acuracy on training set", value="{:.2%}".format(round(accuracy_score(y_train, y_pred_train),3)))
+        if accuracy_score(y_train, y_pred_train) >= 0.9:
+            st.write("The accuracy score is high and shows a great prediction power")
+
+    if accuracy_score(y_train, y_pred_train) - accuracy_score(y_test, y_pred_test) > 0.4:
+        st.warning("The model is overfitting")
+
+    elif accuracy_score(y_train, y_pred_train) - accuracy_score(y_test, y_pred_test) < -0.4:
+        st.warning("The model is underfitting")
+
+    else:
+        st.success("The model is neither overfitting nor underfitting",icon="✅")
+
+    st.write("____________________________________________________________________________________________________________________")
+    with st.expander('performance'):
+        st.markdown("""<h1 style="font-family: 'Times New Roman', Times, serif; font-size: 40px; color: #ff4d4d; font-weight: bold;">Model Performance</h1>""", unsafe_allow_html=True)
+        st.container()
+        col1,col2,col3, col4 = st.columns((1,2,2,3))
+        with col2:
+            st.title("Confusion Matrix")
+            st.table(confusion_matrix(y_test, y_pred_test))
+        with col1:
+            st.write("")
+        with col4:
+            st.title("Classification Report") 
+            st.text(classification_report(y_test, y_pred_test))
+        with col3:
+            st.write("")
+    st.write("____________________________________________________________________________________________________________________")
+
+
+    
+
+
+    st.write("____________________________________________________________________________________________________________________")
+    with st.expander('plots'):
+        st.markdown("""<h1 style="font-family: 'Times New Roman', Times, serif; font-size: 40px; color: #ff4d4d; font-weight: bold;">Plots</h1>""", unsafe_allow_html=True)
+        st.container()
+        col1, col2 = st.columns(2)
+        with col1:
+            st.title("ROC Curve")
+            fig = plt.plot()
+            RocCurveDisplay.from_estimator(lr, x_test, y_test)
+            st.pyplot(fig)
+        with col2:
+            st.write("")
+    st.write("____________________________________________________________________________________________________________________")
+
+          
+
+    
+    
+# -----------------------------------------------------
+# ---Random Forest---
+# -----------------------------------------------------
+
+def random_forest():
+    rf = RandomForestClassifier(n_estimators=200, max_depth=3, random_state=42)
+    rf.fit(x_train, y_train)
+    y_pred_test = rf.predict(x_test)
+    y_pred_train = rf.predict(x_train)
+
+    st.write("____________________________________________________________________________________________________________________")
+    st.markdown("""<h1 style="font-family: 'Times New Roman', Times, serif; font-size: 40px; color: #ff4d4d; font-weight: bold;">Model Evaluation</h1>""", unsafe_allow_html=True)
+    st.write("____________________________________________________________________________________________________________________")
+    st.container()
+    col1 , col2 = st.columns((3,2))
+    with col1:
+        # st.markdown("## Acuracy on test set:")
+        st.metric(label="Acuracy on test set", value="{:.2%}".format(round(accuracy_score(y_test, y_pred_test),3)))
+        if accuracy_score(y_test, y_pred_test) >= 0.9:
+            st.write("The accuracy score is high and shows a great prediction power")
+
+    with col2:
+        # st.markdown("## Acuracy on training set")
+        # st.write("{:.2%}".format(round(accuracy_score(y_train, y_pred_train),3)))
+        st.metric(label="Acuracy on training set", value="{:.2%}".format(round(accuracy_score(y_train, y_pred_train),3)))
+        if accuracy_score(y_train, y_pred_train) >= 0.9:
+            st.write("The accuracy score is high and shows a great prediction power")
+
+    if accuracy_score(y_train, y_pred_train) - accuracy_score(y_test, y_pred_test) > 0.4:
+        st.warning("The model is overfitting")
+
+    elif accuracy_score(y_train, y_pred_train) - accuracy_score(y_test, y_pred_test) < -0.4:
+        st.warning("The model is underfitting")
+
+    else:
+        st.success("The model is neither overfitting nor underfitting",icon="✅")
+
+    st.write("____________________________________________________________________________________________________________________")
+    with st.expander('performance'):
+        st.markdown("""<h1 style="font-family: 'Times New Roman', Times, serif; font-size: 40px; color: #ff4d4d; font-weight: bold;">Model Performance</h1>""", unsafe_allow_html=True)
+        st.container()
+        col1,col2,col3, col4 = st.columns((1,2,2,3))
+        with col2:
+            st.title("Confusion Matrix")
+            st.table(confusion_matrix(y_test, y_pred_test))
+        with col1:
+            st.write("")
+        with col4:
+            st.title("Classification Report") 
+            st.text(classification_report(y_test, y_pred_test))
+        with col3:
+            st.write("")
+    st.write("____________________________________________________________________________________________________________________")
+
+    
+    
+    
+    st.write("____________________________________________________________________________________________________________________")
+    with st.expander('plots'):
+        st.markdown("""<h1 style="font-family: 'Times New Roman', Times, serif; font-size: 40px; color: #ff4d4d; font-weight: bold;">Plots</h1>""", unsafe_allow_html=True)
+        st.container()
+        col1, col2 = st.columns(2)
+        with col1:
+            st.title("ROC Curve")
+            fig = plt.plot()
+            RocCurveDisplay.from_estimator(rf, x_test, y_test)
+            st.pyplot(fig)
+        with col2:
+            imp = pd.Series(rf.feature_importances_, index=x.columns)  
+            st.title("Feature Importance")
+            fig = plt.figure()
+            imp.nlargest(8).plot(kind='barh').invert_yaxis()
+            st.pyplot(fig)
+    st.write("____________________________________________________________________________________________________________________")
+
+    
+
+# -----------------------------------------------------    
+# ---Naive Bayes---
+# -----------------------------------------------------
+
+def naive_bayes():
+    nb = GaussianNB()
+    nb.fit(x_train, y_train)
+    y_pred_test = nb.predict(x_test)
+    y_pred_train = nb.predict(x_train)
+
+    st.write("____________________________________________________________________________________________________________________")
+    st.markdown("""<h1 style="font-family: 'Times New Roman', Times, serif; font-size: 40px; color: #ff4d4d; font-weight: bold;">Model Evaluation</h1>""", unsafe_allow_html=True)
+    st.write("____________________________________________________________________________________________________________________")
+    st.container()
+    col1 , col2 = st.columns((3,2))
+    with col1:
+        # st.markdown("## Acuracy on test set:")
+        st.metric(label="Acuracy on test set", value="{:.2%}".format(round(accuracy_score(y_test, y_pred_test),3)))
+        if accuracy_score(y_test, y_pred_test) >= 0.9:
+            st.write("The accuracy score is high and shows a great prediction power")
+
+    with col2:
+        # st.markdown("## Acuracy on training set")
+        # st.write("{:.2%}".format(round(accuracy_score(y_train, y_pred_train),3)))
+        st.metric(label="Acuracy on training set", value="{:.2%}".format(round(accuracy_score(y_train, y_pred_train),3)))
+        if accuracy_score(y_train, y_pred_train) >= 0.9:
+            st.write("The accuracy score is high and shows a great prediction power")
+
+    if accuracy_score(y_train, y_pred_train) - accuracy_score(y_test, y_pred_test) > 0.4:
+        st.warning("The model is overfitting")
+
+    elif accuracy_score(y_train, y_pred_train) - accuracy_score(y_test, y_pred_test) < -0.4:
+        st.warning("The model is underfitting")
+
+    else:
+        st.success("The model is neither overfitting nor underfitting",icon="✅")
+
+    st.write("____________________________________________________________________________________________________________________")
+    with st.expander('performance'):
+        st.markdown("""<h1 style="font-family: 'Times New Roman', Times, serif; font-size: 40px; color: #ff4d4d; font-weight: bold;">Model Performance</h1>""", unsafe_allow_html=True)
+        st.container()
+        col1,col2,col3, col4 = st.columns((1,2,2,3))
+        with col2:
+            st.title("Confusion Matrix")
+            st.table(confusion_matrix(y_test, y_pred_test))
+        with col1:
+            st.write("")
+        with col4:
+            st.title("Classification Report") 
+            st.text(classification_report(y_test, y_pred_test))
+        with col3:
+            st.write("")
+    st.write("____________________________________________________________________________________________________________________")
+    st.write("____________________________________________________________________________________________________________________")
+    with st.expander('plots'):
+        st.markdown("""<h1 style="font-family: 'Times New Roman', Times, serif; font-size: 40px; color: #ff4d4d; font-weight: bold;">Plots</h1>""", unsafe_allow_html=True)
+        st.container()
+        col1, col2 = st.columns(2)
+        with col1:
+            st.title("ROC Curve")
+            fig = plt.plot()
+            RocCurveDisplay.from_estimator(nb, x_test, y_test)
+            st.pyplot(fig)
+        with col2:
+            st.write("")
+    st.write("____________________________________________________________________________________________________________________")
+
+    
+    
